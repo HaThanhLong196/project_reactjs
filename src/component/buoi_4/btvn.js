a747339ec66bedb033861a2c4f45064e69474412
Log <hatthanhlong1906@gmail.com>
2025-01-08 19:08:59 +0700

    sua file btvn

--- src/component/buoi_4/btvn.js
+++ src/component/buoi_4/btvn.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
 
-function ProductItemHome () {
+const URL =
+  "https://cdn2.fptshop.com.vn/unsafe/750x0/filters:quality(100)/iphone_16_pro_max_desert_titan_3552a28ae0.png";
+
+const ProductItem = ({ product }) => {
+  const { title, img, description, price } = product;
   return (
-    <div></div>
-  )
-}
+    <div
+      style={{
+        width: "350px",
+        textAlign: "center",
+        border: "2px solid #e5e5e5",
+      }}
+    >
+      <div
+        style={{
+          padding: "5px",
+          background: "#fff",
+          borderBottom: "2px solid #e5e5e5",
+        }}
+      >
+        <img style={{ width: "100%" }} src={img} alt="san-pham" />
+      </div>
+      <div style={{ padding: "10px 5px" }}>
+        <h2>{title}</h2>
+        <p style={{ margin: "8px 0px " }}>{description} </p>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default ProductItemHome;
+export default ProductItem;
+
+//1. đẩy code lên ntn? push
+// - push những thay đổi gì & push lên nhánh nào
+// - git add . : thêm những thay đổi vào trong commit
+// - git commit -m"mô tả về ngắn gọn về những thay đổi"
+// - git push origin <tên nhánh>
+
+// Server: 1-2-3-4-5-6-7-8-9-10
+// Local: 1-2-3-4-5-6-7-8-9-10
+
+//2. kéo code về ntn? pull
+// - pull từ nhánh nào
+// - git pull origin <ten nhanh> -> git pull origin master
+
+// git pull orign main
+// main A: 1-2-3-4-5-6-7-8-9-10
+
+// Server: 1-2-3-4-5-6-7-8-9-10
+
+// main B: 1-2-3-4-5-6-7-8-11-12
